f65d683e4fe7fdfbaa905eb403c5dc324bce9fc4
Adrian Adiaconitei <adrian@hipo.ro>
2026-02-04 22:14:13 +0200

    curs1

---
+++ curs1/js/ap2.js
@@ -0,0 +1,1 @@
+alert('Salut din fisier extern');
---
+++ curs1/js/ap3.js
@@ -0,0 +1,17 @@
+let x = 10;
+let y = 20;
+let z = x + y;
+let salut = 'Salut ';
+let reducere = true
+console.log(z);
+const TVA = 24;
+let pret = 100;
+let pretTVA = pret + (pret * TVA / 100);
+console.log(pretTVA);
+
+let num1 = 15;
+num1 = num2
+let num2 = 4;
+let impartire = num1 / num2; //
+let rest = num1 % num2; //
+console.log('Rezultatul impartirii lui ' + num1 + ' la ' + num2 + ' este ' + impartire + ' iar restul este ' + rest);
